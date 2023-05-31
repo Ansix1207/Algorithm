1de2c43d482b9426eddf02957480728618d28959
Ansix1207 <choiansik98@naver.com>
2023-05-31 09:35:44 +0900

    17413번 단어 뒤집기2

--- src/BaekJoon/boj_17413.java
+++ src/BaekJoon/boj_17413.java
@@ -10,28 +10,29 @@ public class boj_17413 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         LinkedList<Character> stack = new LinkedList<Character>();
+
         char[] c_arr = br.readLine().toCharArray();
         boolean tag = false;
         for (int i = 0; i < c_arr.length; i++) {
-            if(c_arr[i] == '<' || tag){
-                while(!stack.isEmpty()){
-                    System.out.print(stack.pop());
+            if(c_arr[i] == '<' || tag){//꺽쇠를 만났을때
+                while(!stack.isEmpty()){//스택이 비지 않았으면
+                    System.out.print(stack.pop());//스택에 있는거 모두 출력
                 }
-                tag=true;
+                tag=true;//꺽쇠만난거 체크
                 System.out.print(c_arr[i]);
                 if (c_arr[i] == '>') {
-                    tag=false;
+                    tag=false;//태그 끝나면 tag=false
                 }
             }
-            else{
-                if(c_arr[i]==' '){
-                    while(!stack.isEmpty()){
+            else{//꺽쇠가 아닌 일반
+                if(c_arr[i]==' '){//공백을 만나면 모두 팝
+                    while(!stack.isEmpty()){//스택 안비었으ㅕㄴ
                         System.out.print(stack.pop());
                     }
-                    System.out.print(' ');
+                    System.out.print(' ');//출력 형식 맞추기
                 }
                 else{
-                    stack.push(c_arr[i]);
+                    stack.push(c_arr[i]);//스택에 넣기
                 }
             }
         }
