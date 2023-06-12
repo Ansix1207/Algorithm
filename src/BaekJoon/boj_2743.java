ec517cafa8a1fbb0f53eab8f1cefee92dd259699
Ansix1207 <choiansik98@naver.com>
2023-06-12 11:46:27 +0900

    2743번 단어길이 재기

--- src/BaekJoon/boj_2743.java
+++ src/BaekJoon/boj_2743.java
@@ -1,2 +1,14 @@
-package BaekJoon;public class boj_2743 {
+package BaekJoon;
+
+// TODO: 2023/06/12 2743번 단어길이 재기 (브론즈5) https://www.acmicpc.net/problem/2743
+// TODO: 2023/06/12 !!성공!!
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
 }
