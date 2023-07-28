e320660bd3e9e9eba6dd7a567854fdbe29289292
Ansix1207 <choiansik98@naver.com>
2023-07-29 00:43:33 +0900

    25083번 새싹

---
+++ src/BaekJoon/boj_25083_새싹.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.io.IOException;
+
+// TODO: 2023/07/28 25083번 새싹 https://www.acmicpc.net/problem/25083
+// TODO: 2023/07/28 !!성공!! 뭐 이런 문제가 다있냐 ㅋㅋ
+public class boj_25083_새싹 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
