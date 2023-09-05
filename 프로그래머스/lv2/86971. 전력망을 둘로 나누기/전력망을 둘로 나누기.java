3ab558438f3cc77c4b72675d81d9fdc91a1f778d
최안식 <112413977+Ansix1207@users.noreply.github.com>
2023-09-05 13:46:01 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.98 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,15 +2,7 @@ import java.util.*;
 class Solution {
         static int arr[][];
         static boolean visit[];
-        public int[][] arrayCopy(int[][] array){
-            int[][] result = new int[array.length][array.length];
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    result[i][j] = array[i][j];
-                }
-            }
-            return result;
-        }
+    
         public int solution(int n, int[][] wires) {
             int answer = Integer.MAX_VALUE;
             arr = new int[n+1][n+1];
@@ -24,19 +16,16 @@ class Solution {
             //모든 경우의수 따지기 위해서 해당부분 제거한 배열을 넣음
             for (int i = 0; i < wires.length; i++) {
                 visit = new boolean[n+1];
-                int[][] tarr = arrayCopy(arr);
                 int low = wires[i][0];
                 int column = wires[i][1];
-                tarr[low][column] = 0;
-                tarr[column][low] = 0;
-                int bfs1 = bfs(tarr, low);
-                System.out.printf("======= low = %d, column = %d\n",low, column);
-                System.out.println("bfs1 = " + bfs1);
-                int bfs2 = bfs(tarr, column);
-                System.out.println("bfs2 = " + bfs2);
+                arr[low][column] = 0;
+                arr[column][low] = 0;
+                int bfs1 = bfs(arr, low);
+                int bfs2 = bfs(arr, column);
+                arr[low][column] = 1;
+                arr[column][low] = 1;
                 answer = Math.min(answer, Math.abs(bfs1-bfs2));
             }
-            System.out.println("answer = " + answer);
             return answer;
         }
 
