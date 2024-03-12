0abcbe0ecdba55f54426c5afff2cf3608ddf3d37
최안식 <112413977+Ansix1207@users.noreply.github.com>
2024-03-12 23:56:43 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 3040 ms, Memory: 361272 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            PriorityQueue<Long> pq = new PriorityQueue<>();
+            for (String arg : br.readLine().split(" ")) {
+                pq.add(Long.parseLong(arg));
+            };
+            Long sum = 0L;
+            while(pq.size()>=2){
+                Long x = pq.poll();
+                Long y = pq.poll();
+                pq.add(x+y);
+                sum += x+y;
+            }
+            System.out.println(sum);
+        }
+    }
+}
